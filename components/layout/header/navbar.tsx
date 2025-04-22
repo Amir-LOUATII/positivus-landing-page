414fe161fca305b62ccdbd546661973ba90751fc
Amir_Louati25 <ameer.louati@gmail.com>
2025-04-22 23:52:53 +0100

    fix: update Navbar header to ensure full width layout

--- components/layout/header/navbar.tsx
+++ components/layout/header/navbar.tsx
@@ -4,7 +4,7 @@ import Nav from "./nav";
 
 export default function Navbar() {
   return (
-    <header className="h-[68px] py-2">
+    <header className="h-[68px] py-2 w-full">
       <div
         className="container mx-auto px-2
        flex justify-between items-center gap-10"
