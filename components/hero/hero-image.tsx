364c2994b006736104f7c74825e84e43f5a2338e
Amir_Louati25 <ameer.louati@gmail.com>
2025-05-02 01:02:02 +0100

    style: restructure HeroImage component layout and enhance image properties

--- components/hero/hero-image.tsx
+++ components/hero/hero-image.tsx
@@ -1,7 +1,17 @@
 import Image from "next/image";
-import React from "react";
 
 import heroImage from "@/assets/images/heroImage.svg";
+
 export default function HeroImage() {
-  return <Image src={heroImage} alt="Hero image" className="max-w-full " />;
+  return (
+    <div className="w-full max-md:max-w-[340px] mx-auto px-4">
+      <Image
+        src={heroImage}
+        alt="Hero image"
+        className="w-full h-auto object-contain"
+        sizes="(max-width: 768px) 100vw, 360px"
+        priority
+      />
+    </div>
+  );
 }
